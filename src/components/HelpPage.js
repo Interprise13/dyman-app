9fc156b46e695895d2b0b8efc3411cb08fed1e27
Interprise13 <r.marsh@live.com.au>
2019-03-20 21:46:43 +1100

    Initial Commit

---
+++ src/components/HelpPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const HelpPage = () => (
+    <div>
+        <p>This is from add help component</p>
+    </div>
+);
+
+export default HelpPage;
---
+++ src/playground/destructuring.js
@@ -0,0 +1,76 @@
+//const person = {
+//    name: 'Ryan',
+//    age: 27,
+//    location: {
+//        city: 'Brisbane',
+//        temp: 36
+//    }
+//};
+//
+//const {name: firstName = 'Anonymous', age } = person
+//
+//
+//
+//
+//console.log(`${firstName} is ${age}.`);
+//
+//
+//
+//const { city, temp: temperature } = person.location
+//
+//
+//if(city && temperature) {
+//    console.log(`Its ${temperature} in ${city}.`);
+//}
+
+
+//const book = {
+//    title: 'Ego is the Enemy',
+//    author: 'Ryan Holiday',
+//    publisher: {
+//        name:'Penguin'
+//    }
+//};
+//
+//const {name: publisherName = 'Self Published'} = book.publisher;
+//
+//
+//console.log(publisherName);
+
+
+//Array destructuring
+
+//const address = ['1299 S Juniper Street', 'philidelphia', 'pennsylvania', '19147'];
+//
+//
+//const [, city, state = 'New York'] = address;
+//
+//console.log(`Your are in ${city} ${state}.`)
+
+
+
+
+const item = ['Coffee (hot)', '$2.00', '$2.50', '$2.75'];
+
+const [coffee,,price] = item;
+
+console.log(`A medium ${coffee} costs ${price}`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
